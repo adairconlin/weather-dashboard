b2e18cfc8e89c899a62283053e408d90ecdeec00
adairconlin <adairconlin@gmail.com>
2021-11-07 14:50:26 -0500

    Add OpenWeatherMap API and start generating data onto page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-console.log("test");
+let getCurrentWeather = function(city, data) {
+    let location = document.querySelector("#location");
+    let temperature = document.querySelector("#temp");
+    let wind = document.querySelector("#wind");
+    let humidity = document.querySelector("#humidity");
+    location.textContent = city;
+    temperature.textContent = data.main.temp + "°F";
+    wind.textContent = data.wind.speed + "mph";
+    humidity.textContent = data.main.humidity + "%";
+};
+
+let formSubmit = function(event) {
+    event.preventDefault();
+    let city = document.querySelector("#city").value;
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=198d07e9a046131e4583b2665e1187a0";
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                getCurrentWeather(city, data);
+            })
+        } else {
+            console.log("Error: City Not Found");
+        }
+    }); 
+};
+
+document.querySelector("#submit").addEventListener("click", function() {
+    let input = document.querySelector("#city").value;
+    input.textContent = "";
+    formSubmit();
+});
